7a75ff059d45cf3b1b4dcff66cfdb5d8ffa72ba7
Tim <sutton.timothy85@gmail.com>
2024-03-21 18:32:19 -0400

    Added 'launch' folder for future launch files. Made changes to setup.py to allow launch files

---
+++ cubesat_py/launch/sat.launch.py
@@ -0,0 +1,30 @@
+# example.launch.py
+
+
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.actions import GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import TextSubstitution
+from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
+
+
+def generate_launch_description():
+
+
+    rs_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('realsense2_camera'), 'launch', 'rs_launch.py'
+        )]),
+        #launch_arguments={'pointcloud.enable': 'true'}.items()
+    )
+    return LaunchDescription([
+
+    ])

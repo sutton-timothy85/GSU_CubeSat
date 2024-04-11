8caabfd39e2878c50e5ccb213eedd98368f43f93
Tim <sutton.timothy85@gmail.com>
2024-04-11 15:56:55 -0400

    added camera test script

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import burnwire
 import adafruit_mpl3115a2
 import adafruit_icm20x
 import adafruit_mcp9808
-
+from picamera2 import Picamera2, Preview
 activated = False
 
 i2c = board.I2C()
-
+picam = Picamera2()
 altimeter = adafruit_mpl3115a2.MPL3115A2(i2c)
 imu = adafruit_icm20x.ICM20948(i2c)
 temp = adafruit_mcp9808.MCP9808(i2c)
-
+picam.start()
 
 altimeter.sealevel_pressure = 1022.5
 
@@ -24,9 +24,10 @@ def collect_data():
 
     data = [temp_RW, acceleration, atitude, altitude]
     deliver(data)
+    print(str(data))
 
-
-
+def capture_image():
+    picam.capture_file("image")
 
 def deliver(data):
     xbee = 0
@@ -51,4 +52,5 @@ def main():
 
 
     except KeyboardInterrupt:
+        picam.close()
         print("Shutting Down")
